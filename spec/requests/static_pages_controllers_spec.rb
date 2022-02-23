9fe703e99f75d416ed06cb217ce8e993d2cb9491
Louie Young <louie.young@katalyst.com.au>
2022-02-23 16:14:50 +1030

    Added contact static page and rspec view test Added Root to rspec controller test

--- spec/requests/static_pages_controllers_spec.rb
+++ spec/requests/static_pages_controllers_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe 'StaticPagesControllers', type: :request do
   subject { action && response }
 
+  describe "GET #root" do
+    let(:action) { get root_path }
+    it { is_expected.to have_http_status(:success)}
+  end
+
   describe "GET #home" do
     let(:action) { get home_path }
     it { is_expected.to have_http_status(:success)}
@@ -15,6 +20,11 @@ RSpec.describe 'StaticPagesControllers', type: :request do
 
   describe "GET #about" do
     let(:action) { get about_path }
+    it { is_expected.to have_http_status(:success)}
+  end
+
+  describe "GET #contact" do
+    let(:action) { get contact_path }
     it { is_expected.to have_http_status(:success)}
   end
 end
