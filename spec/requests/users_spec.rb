4569a3fa1e192e7ed0ceee861b5f814996363403
Louie Young <louie.young@katalyst.com.au>
2022-02-25 15:55:15 +1030

    CH-5: Added Users controller and Signup view

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  subject { action && response }
+
+  describe "GET #new" do
+    let(:action) { get root_path }
+    it { is_expected.to have_http_status(:success) }
+  end
+end
