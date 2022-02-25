4fd46df6d04e5dd00c86836e784f3008e0d2bea7
Louie Young <louie.young@katalyst.com.au>
2022-02-25 15:56:29 +1030

    CH-5: Updated primary Application view with partials

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get '/about',   to: 'static_pages#about'
   get '/help',    to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
+
+  get '/new',     to: 'users#new', as: 'signup'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
