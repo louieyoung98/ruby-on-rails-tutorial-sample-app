8ee5085a2d9ba00937cff5eb6cd8f48f87032a0f
Louie Young <louie.young@katalyst.com.au>
2022-03-03 16:17:03 +1030

    CH-7: Updated User Views
    - Added new route form view and styling

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,9 @@
 
 FactoryBot.define do
   factory :user do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    username { Faker::Internet.unique.username }
+    first_name { Faker::Name.first_name[0..49] }
+    last_name { Faker::Name.last_name[0..49] }
+    username { Faker::Internet.unique.username[0..14] }
     password { "test_password" }
     phone_number { Faker::PhoneNumber.phone_number_with_country_code }
     email { "#{first_name}.#{last_name}@example.com" }
