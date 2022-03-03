e311144f23e61fa295d292e19076ef4c018a6165
Louie Young <louie.young@katalyst.com.au>
2022-03-03 16:14:16 +1030

    Conf / Refactor: Ran Rubocop over Routes
    - Changed occurences of 'signup' to 'sign up'

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root "static_pages#home"
 
   resources :users
 
-  get '/home',    to: 'static_pages#home'
-  get '/about',   to: 'static_pages#about'
-  get '/help',    to: 'static_pages#help'
-  get '/contact', to: 'static_pages#contact'
+  get "/home",    to: "static_pages#home"
+  get "/about",   to: "static_pages#about"
+  get "/help",    to: "static_pages#help"
+  get "/contact", to: "static_pages#contact"
 
-  get '/new',     to: 'users#new', as: 'signup'
+  get "/signup",  to: "users#new", as: "signup"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
